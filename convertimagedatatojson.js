ce8d8173ebf150982471267a4033c7d0864fadbc
ankitmishra23 <42620135+ankitmishra23@users.noreply.github.com>
2020-06-30 20:52:19 +0530

    Add files via upload

---
+++ convertimagedatatojson.js
@@ -0,0 +1,50 @@
+let technical_specifications={
+    "dimensions":
+    {
+        "overall_length":3500,
+        "overall_width":1600,
+        "overall_height":1490,
+        "wheelbase":2360,
+        "track_width":
+        {
+            "front":1405,
+            "rear":1400
+        },
+        "minimum_turning_radius":4.5,
+        "minimum_ground_clearance":170
+    },
+    "capacities":
+    {
+            "seating_capacity":5,
+            "fuel_tank_capacity":35
+    },
+    "engine":
+    {
+        "type":"KB-Series",
+        "number_of_cylinders":3,
+        "number_of_valves":12,
+        "capacity":998,
+        "bore_x_stroke":"73.0 x 79.5",
+        "compression_ratio":"10:1",
+        "maximum_power":"67/6.200",
+        "maximum_torque":"90/3.500",
+        "fuel_distribution":"Multipoint Injection"
+    },
+    "transmission":
+    {
+        "type":"5-speed MT"
+    },
+    "chasis":
+    {
+        "streeing":["Rack & Pinion","Power assisted"],
+        "brakes":[{"front":"Ventilated discs"},{"Rear":"Drums"}],
+        "suspension":[{"front":"MacPherson strut & coil spring"},{"rear":"isolated trailing link & coil spring"}],
+        "shock_absorbers":"Gas filled",
+        "tyre(tubeless)":"155/80R13"
+    },
+    "weights":
+    {
+        "kerb_weight":"860-880",
+        "gross_vechicle_weight":1320
+    }
+}
